2254b585062d1c9d4b3d2169f39bc36e41337fe9
Filipe Estacio <f.estacio@gmail.com>
2021-06-18 13:09:22 +0100

    added web-admin-api to prometheus

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,4 @@
 echo "Running Confd"
 echo "dashboard: \"$(bcrypt-tool hash $DASHBOARD_PASSWORD 12)\"" > /etc/confd/hashed.yaml
 # Use here the backend you want or just delete the -backend parameter if you want to use just envvars
-/bin/confd -onetime -backend file -file /etc/confd/hashed.yaml && echo "Running Prometheus" && /bin/prometheus --config.file=/etc/prometheus/confd-prometheus.yml --web.config.file=/etc/prometheus/confd-web-config.yml
+/bin/confd -onetime -backend file -file /etc/confd/hashed.yaml && echo "Running Prometheus" && /bin/prometheus --config.file=/etc/prometheus/confd-prometheus.yml --web.config.file=/etc/prometheus/confd-web-config.yml --web.enable-admin-api
